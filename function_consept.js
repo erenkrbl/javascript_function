65d6fa9fde45fadc708d102578bc79518a4824fd
Joseph Abraham <ekurubal24@gmail.com>
2021-03-17 23:50:17 +0300

    Added writting with for in the function

--- function_consept.js
+++ function_consept.js
@@ -1,17 +1,36 @@
 // Why need function
 
-let total1 = 0;
-let total2 = 0;
+// let total1 = 0;
+// let total2 = 0;
 
-for (i = 0; i <= 10; i++) {
-    total1 = total1 + i;
-}
-console.log(total1);
+// for (i = 0; i <= 10; i++) {
+//     total1 = total1 + i;
+// }
+// console.log(total1);
 
-for (i = 30; i <= 35; i++) {
-    total2 = total2 + i;
+// for (i = 30; i <= 35; i++) {
+//     total2 = total2 + i;
+// }
+// console.log(total2);
+
+// Displaying the code written with for in the function
+
+
+let overallTotal1 = sumTotal(0, 10);  
+let overallTotal2 = sumTotal(30, 35);
+
+function sumTotal (startNumber, finishNumber) {
+    let total = 0;
+    
+    for (i = startNumber; i <= finishNumber; i++ ) {
+        total = total + i;
+    }
+    console.log("Total : " + total);
+    return total;
 }
-console.log(total2);
+console.log("overallTotal1 : " + overallTotal1 + " overallTotal2 : " + overallTotal2);
+
+
 
 function hiSay () {
     console.log("Hello Everybody");
