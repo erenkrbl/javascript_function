277fa4057e4561656838125ed84ded82c80554eb
Joseph Abraham <ekurubal24@gmail.com>
2021-03-18 23:28:26 +0300

    Operation of reference type data

--- refrence_object_array.js
+++ refrence_object_array.js
@@ -11,3 +11,25 @@ let newMyName = myName;
 
 myName = "Martin";
 console.log(myName, newMyName);
+
+// refernce data type
+
+let colors = ["Black", "White"];
+let newColors = colors;
+
+colors = ["Black", "White"];
+
+colors.push('Blue');
+console.log(colors, newColors);
+
+let students = {
+    firstname : 'Khan',
+    lastName : 'Taylor',
+    age : 32
+}
+
+let newStudents = students;
+students.age = 36;
+
+
+console.log(students, newStudents);
