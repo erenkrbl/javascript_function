d8d3c7b75c20d9a0fc42644a0c2d1f219a19dcb1
Joseph Abraham <ekurubal24@gmail.com>
2021-03-18 19:59:47 +0300

    Added shift and unshift method

--- array_method.js
+++ array_method.js
@@ -4,14 +4,22 @@ let myFriends = ["Oliver", "Jack", "Tom"];
 console.log(myFriends.toString());
 console.log(myFriends.join(" | "));
 
-// psh() element add
+// psh() Add elements to the end of the array
 
 let addElement = myFriends.push("Adam");
 console.log(myFriends.toString() + " The number of elements of the array : " + addElement);
 
-// pop() element delete
+// pop() delete the last element of the array
 
-let deleteElemnet = myFriends.pop();
-console.log(myFriends.toString() + " Delete elements of the array : " + deleteElemnet);
+let delElemnet = myFriends.pop();
+console.log(myFriends.toString() + " Deleted element : " + delElemnet);
 
+// shift() delete the first element of the array
 
+let delFirstElement = myFriends.shift();
+console.log(myFriends.toString() + " Deleted element : " + delFirstElement);
+
+// unshift () Add elements to the beginning of the array
+
+let addFirstElement = myFriends.unshift("Harold");
+console.log(myFriends.toString() + " Added element : " + addFirstElement);
