6f0c401a1fbf77898abb179db8286060094b4fdf
Joseph Abraham <ekurubal24@gmail.com>
2021-03-18 23:13:49 +0300

    Operation of primitive type data

---
+++ refrence_object_array.js
@@ -0,0 +1,13 @@
+// primitive data type
+
+let age = 32;
+let newAge = age;
+
+age = 40;
+console.log(age, newAge);
+
+let myName = "Eric";
+let newMyName = myName;
+
+myName = "Martin";
+console.log(myName, newMyName);
