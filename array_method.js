83245e2a9c17318b774e2fa32fe15ae3512e2dd7
Joseph Abraham <ekurubal24@gmail.com>
2021-03-18 19:08:49 +0300

    added push and pop method

--- array_method.js
+++ array_method.js
@@ -3,3 +3,15 @@ let myFriends = ["Oliver", "Jack", "Tom"];
 // toString()- join()
 console.log(myFriends.toString());
 console.log(myFriends.join(" | "));
+
+// psh() element add
+
+let addElement = myFriends.push("Adam");
+console.log(myFriends.toString() + " The number of elements of the array : " + addElement);
+
+// pop() element delete
+
+let deleteElemnet = myFriends.pop();
+console.log(myFriends.toString() + " Delete elements of the array : " + deleteElemnet);
+
+
