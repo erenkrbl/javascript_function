0de1f058b25ad426c686cfda54ee6b6afaaa3e55
Joseph Abraham <ekurubal24@gmail.com>
2021-03-19 19:04:49 +0300

    Added higher order function

--- higher_order_callback.js
+++ higher_order_callback.js
@@ -1,4 +1,4 @@
-
+/*
 let myArray = [1, 2, 3];
 
 const arrayCopyTwoMultiply = function(array) {
@@ -30,3 +30,42 @@ arrayCopyTwoMultiply(myArray);
 arrayCopyTwoDivision(myArray);
 arrayCopyThreeAdd(myArray);
 
+*/
+
+let myArray = [1, 2, 3];
+
+// first class function
+const twoWithMultiply = function(number) {
+    return number * 2;
+}
+
+const twoWithDivision = function(number) {
+    return number / 2;
+}
+
+const threeAdd = function(number) {
+    return number + 3;
+}
+
+
+const transactionsAll = function(array, transactions) {
+    let temporaryArray = [];
+    for (i = 0; i < array.length; i++) {
+        //temporaryArray[i] = array[i] * 2;
+        //temporaryArray[i] = array[i] / 2;
+        //temporaryArray[i] = array[i] + 3;
+        temporaryArray[i] = transactions(array[i]);
+    }
+    console.log(temporaryArray);
+    
+}
+
+// higher order order function :Parametre olarak fonksiyon alan ve/veya
+// return olarak fonksiyon döndüren fonksiyolardır.
+// izi ilemleri higher order fonksiyondur.
+
+transactionsAll(myArray, twoWithMultiply);
+transactionsAll(myArray, twoWithDivision);
+transactionsAll(myArray, threeAdd)
+
+console.log(myArray);
