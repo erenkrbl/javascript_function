06fd0c3738096b8482e7fef6c1da57ecf430a0f0
Joseph Abraham <ekurubal24@gmail.com>
2021-03-19 23:46:10 +0300

    Added  writing with function

---
+++ filter.js
@@ -0,0 +1,34 @@
+
+
+const plants = [
+    {pName : 'grape', kind : 'fruits' },
+    {pName : 'strawberry', kind : 'fruits' },
+    {pName : 'banana', kind : 'fruits' },
+    {pName : 'spinach', kind : 'vegetables' },
+    {pName : 'celery', kind : 'vegetables' },
+];
+
+function findFruits () {
+    const newArray = [];
+    for (i = 0; i < plants.length; i++) {
+        if (plants[i].kind === 'fruits') {
+            newArray.push(plants[i]);
+        }
+    }
+    return newArray;
+}
+
+
+function findVegetables () {
+    const newArray = [];
+    for (i = 0; i < plants.length; i++) {
+        if (plants[i].kind === 'vegetables') {
+            newArray.push(plants[i]);
+        }
+    }
+    return newArray;
+}
+
+console.log(plants);
+console.log(findFruits());
+console.log(findVegetables());
