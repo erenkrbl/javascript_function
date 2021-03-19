9e18df2e7eef264b5d84d8362de076b16b1368ad
Joseph Abraham <ekurubal24@gmail.com>
2021-03-19 21:12:32 +0300

    Added map function

---
+++ map_function.js
@@ -0,0 +1,37 @@
+// 
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const newNumbers = numbers.map(function(number) {
+    return number * 2;
+});
+
+console.log(numbers);
+console.log(newNumbers);
+console.log(numbers);
+
+const books = [
+    {bookname : "White Tooth", pageNumber : 34},
+    {bookname : "Blak life", pageNumber : 40},
+    {bookname : "Blue Sky", pageNumber : 58},
+    {bookname : "Fovers", pageNumber : 67},
+];
+
+const bookPageNumbers = books.map(function (book) {
+    return book.pageNumber;
+});
+
+console.log(bookPageNumbers);
+
+const person = [
+    {firstName : 'Oliver', lastName : 'Samba'},
+    {firstName : 'Joseph', lastName : 'Smith'},
+    {firstName : 'John', lastName : 'Zack'},
+];
+
+const ourPerson = person.map(function(personName) {
+    return personName.firstName + " " + personName.lastName;
+});
+
+console.log(ourPerson);
+
