ef80d6e2766b72314a627eecab60c2f9cfc3ab38
Joseph Abraham <ekurubal24@gmail.com>
2021-03-20 21:28:58 +0300

    Solution with classical function

---
+++ reduce.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+
+
+function sumFind () {
+    let sum = 0;
+    for (i = 0; i < numbers.length; i++) {
+        sum = sum + numbers[i];
+    }
+    console.log(sum);
+}
+
+sumFind();
+
+
+
