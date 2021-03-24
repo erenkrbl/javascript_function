3bbe07b13f2f702d152b9bc398f3e43089b0bf05
Joseph Abraham <ekurubal24@gmail.com>
2021-03-24 19:57:09 +0300

    Added finding numbers square

--- helper_func_assignment.js
+++ helper_func_assignment.js
@@ -2,14 +2,20 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // map, filter, reduce, some, every, find
 
-//tek  elemanlların bulunup başka diziye akatrılması
+//tek  elemanlların bulunup başka diziye aktarılması (filter)
+// oluşan yeni dizinin her bir elemanının karesini bul (map)
+// bu dizideki 10 dan büyük olan sayıların toplamını ekrana yazdırın (reduce)
 
 function resultFind(numbers) {
-    const oddFilter = numbers.filter(function(oddNumber) {
-        return oddNumber % 2 !== 0;
+    const oddNumber = numbers.filter(function(number) {
+        return number % 2 !== 0;
     });
-    console.log("odd numbers :" + oddFilter);
+    console.log("odd numbers :" + oddNumber);
 
-    
+    const numberSquare = oddNumber.map(function(number) {
+        return number * number;
+    });
+    console.log(numberSquare);
+
 }
 resultFind(numbers);
