93ebeab34a24072863f66b91af46ab6df797b126
Joseph Abraham <ekurubal24@gmail.com>
2021-03-27 18:03:24 +0300

    Added query selector

--- dom_selector.js
+++ dom_selector.js
@@ -23,3 +23,21 @@ head.innerText = 'New Head';
 
 head.innerText = '<img src="https://source.unsplash.com/100x100" alt="">';
 head.innerHTML = '<img src="https://source.unsplash.com/100x100" alt="">';
+
+// query selector
+
+console.log(document.querySelector('h2'));
+console.log(document.querySelector('#list'));
+console.log(document.querySelector('.liste-item'));
+console.log(document.querySelector('ul li'));
+console.log(document.querySelector('.link.link-google'));
+
+const myList = document.querySelector('li');
+
+myList.style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'green';
+document.querySelector('li:nth-child(2)').style.color = 'orange';
+document.querySelector('li:nth-child(3)').style.color = 'purple';
+document.querySelector('li:nth-child(even)').style.color = 'yellow';
+document.querySelector('li:nth-child(odd)').style.color = 'gray';
