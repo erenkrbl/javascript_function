cff6946096fabb6b90a5392a4ca3c1138477541d
Joseph Abraham <ekurubal24@gmail.com>
2021-03-29 18:45:55 +0300

    Added mouse event

--- dom_events.js
+++ dom_events.js
@@ -1,6 +1,15 @@
 document.querySelector('.link').addEventListener('click', myClick);
 document.querySelector('.link').addEventListener('dblclick', myClick);
 
+document.querySelector('.link').addEventListener('mousedown', myClick);
+document.querySelector('.link').addEventListener('mouseup', myClick);
+
+document.querySelector('.container').addEventListener('mouseenter', myClick);
+document.querySelector('.container').addEventListener('mouseleave', myClick);
+
+document.querySelector('.container').addEventListener('mouseover', myClick);
+document.querySelector('.container').addEventListener('mouseout', myClick);
+
 function myClick (e) {
     let value;
 
