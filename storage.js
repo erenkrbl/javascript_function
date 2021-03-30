766bb379f7a4c5cac97be0ff8532e98677c1dcbb
Joseph Abraham <ekurubal24@gmail.com>
2021-03-30 19:32:01 +0300

    Add element with JSON parse, stringify

--- storage.js
+++ storage.js
@@ -1,13 +1,31 @@
-// local and session storage
+//local and session storage
+ /*
+localStorage.setItem('name', 'Eric');
+localStorage.setItem('age', '35');
 
-// localStorage.setItem('name', 'Eric');
-// localStorage.setItem('age', '35');
-
-// sessionStorage.setItem('city', 'Tiflis');
+sessionStorage.setItem('city', 'Tiflis');
 
 console.log(localStorage.getItem('name'));
 console.log(localStorage.getItem('age'));
 
-//localStorage.removeItem('name');
+localStorage.removeItem('name');
 
 localStorage.clear();
+*/
+
+document.querySelector('#form').addEventListener('submit', function (e) {
+    const newName = document.querySelector('.name').value;
+    let nameArray;
+
+    if (localStorage.getItem('nameList') === null) {
+        nameArray = [];
+    } else {
+        nameArray = JSON.parse(localStorage.getItem('nameList'))
+    }
+
+    nameArray.push(newName);
+
+    localStorage.setItem('nameList', JSON.stringify(nameArray));
+
+    e.preventDefault();
+})
