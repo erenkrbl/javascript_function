537407666b5dc9f82977fbe31ba436e9b95a41e6
Joseph Abraham <ekurubal24@gmail.com>
2021-03-30 19:04:09 +0300

    Added store and delete element

---
+++ storage.js
@@ -0,0 +1,13 @@
+// local and session storage
+
+// localStorage.setItem('name', 'Eric');
+// localStorage.setItem('age', '35');
+
+// sessionStorage.setItem('city', 'Tiflis');
+
+console.log(localStorage.getItem('name'));
+console.log(localStorage.getItem('age'));
+
+//localStorage.removeItem('name');
+
+localStorage.clear();
