40707bb7efed9b16cf979372635a04c87a2c9a1e
Joseph Abraham <ekurubal24@gmail.com>
2021-04-04 20:53:44 +0300

    Added array prototype

--- object_instance.js
+++ object_instance.js
@@ -17,6 +17,7 @@ function Student(fName, LName, age, room) {
 }
 
 Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
 
 Student.prototype.sayHello  = function() { 
     return `Hello I'm ${this.room}. room student ${this.fName} ${this.LName}`;
@@ -36,3 +37,39 @@ console.log(oliver.sayHello());
 const kamala = new Student('Kamala', 'Sue', 17, 9);
 console.log(kamala.sayHello());
 console.log(kamala.sayAge());
+
+/*
+function MyArray () {
+
+}
+
+MyArray.prototype = Object.create(Array.prototype);
+
+const myArray = new MyArray();
+
+myArray.push(1, 2, 34, 'aron');
+
+
+console.log(myArray);
+
+*/
+
+Array.prototype.mySelfMap = function (operation) {
+    const newArray = [];
+    for (i = 0; i < this.length; i++) {
+        newArray.push(operation(this[i], i));
+    }
+    return newArray;
+}
+
+const myArray = [1, 3, 6];
+const newArray = myArray.map(function(number) {
+    return number + 2;
+});
+
+const myNewArray = myArray.mySelfMap(function(number) {
+    return number + 2;
+})
+
+console.log(newArray);
+console.log(myNewArray);
