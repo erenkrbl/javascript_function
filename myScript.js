5a21a955e5d6e47e13e713534d86600dcdce27da
Joseph Abraham <ekurubal24@gmail.com>
2021-05-06 16:21:15 +0300

    Feature made with class

--- myScript.js
+++ myScript.js
@@ -1,12 +1,31 @@
 document.getElementById("submit_btn").addEventListener('click', submitUser);
+document.getElementById("list_btn").addEventListener('click', listUser);
 
+
+
+class User {
+    constructor(userName, userSurname, userAge){
+        this.name = userName;
+        this.surname = userSurname;
+        this.age = userAge;
+    }
+    login() {
+        console.log(this.name, this.surname)
+    }
+}
+const userList = [];
 
 function submitUser(){
-    const user = {
-        name : document.getElementById('name_inp').value,
-        surname : document.getElementById('surname_inp').value,
-        age : document.getElementById('age_inp').value,
-    }
+    const name = document.getElementById('name_inp').value;
+    const surname = document.getElementById('surname_inp').value;
+    const age = document.getElementById('age_inp').value;
 
-    console.log(user);
+    const user = new User(name, surname, age)
+    //console.log(user)
+    userList.push(user)
+}
+
+function listUser(){
+    console.log(userList)
+    userList[1].login();
 }
