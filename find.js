7a7a63e02fa391a97bf2a5554252751131c99e9d
Joseph Abraham <ekurubal24@gmail.com>
2021-05-08 15:22:56 +0300

    Added new example

--- find.js
+++ find.js
@@ -1,31 +1,31 @@
 // find
 
-const persons = [
-    {id : 10, firstName : 'Jason'},
-    {id : 11, firstName : 'Jack'},
-    {id : 12, firstName : 'John'},
-    {id : 14, firstName : 'James'},
-    {id : 15, firstName : 'Jordan'},
-    {id : 16, firstName : 'Jimmy'},
-];
+// const persons = [
+//     {id : 10, firstName : 'Jason'},
+//     {id : 11, firstName : 'Jack'},
+//     {id : 12, firstName : 'John'},
+//     {id : 14, firstName : 'James'},
+//     {id : 15, firstName : 'Jordan'},
+//     {id : 16, firstName : 'Jimmy'},
+// ];
 
 // higher order function
 
-function selfFindFunc(persons, searchValue) {
-    let findElement = undefined;
-    for (let i = 0; i < persons.length; i++) {
-        if (searchValue(persons[i])) {
-            return persons[i];
-        }
-    }
-    return findElement;
-}
+// function selfFindFunc(persons, searchValue) {
+//     let findElement = undefined;
+//     for (let i = 0; i < persons.length; i++) {
+//         if (searchValue(persons[i])) {
+//             return persons[i];
+//         }
+//     }
+//     return findElement;
+// }
 
-const result = selfFindFunc(persons, function(person) {
-    return person.id === 10;
-});
+// const result = selfFindFunc(persons, function(person) {
+//     return person.id === 10;
+// });
 
-console.log(result);
+// console.log(result);
 
 
 /*
@@ -45,3 +45,16 @@ function findMethod (persons) {
 console.log(result);
 console.log(findMethod(persons));
 */
+
+const persons = [
+    {id : 10, firstName : 'Jason', age:23},
+    {id : 11, firstName : 'Jack', age:27},
+    {id : 12, firstName : 'John', age:35},
+    {id : 14, firstName : 'James', age:45},
+    {id : 15, firstName : 'Jordan', age:36},
+    {id : 16, firstName : 'Jimmy', age:35},
+];
+
+// const myPerson = persons.find((person) => person.age === 35);
+const myPerson = persons.find((person) => person.age < 35);
+console.log(myPerson);
