59271ab73b45dfaccc11e0b9845323342820d978
Joseph Abraham <ekurubal24@gmail.com>
2021-05-08 16:19:51 +0300

    Added some and every about example

--- every_some.js
+++ every_some.js
@@ -18,3 +18,20 @@ const someResult = numbers.some(function (number) {
 console.log(everyResult);
 console.log(someResult);
 
+const persons = [
+    {id : 10, isOld: false, fName : 'Jason', age:23},
+    {id : 11, isOld: true, fName : 'Jack', age:27},
+    {id : 12, isOld: false, fName : 'John', age:35},
+    {id : 14, isOld: false, fName : 'James', age:45},
+    {id : 15, isOld: true, fName : 'Jordan', age:36},
+    {id : 16, isOld: false, fName : 'Jimmy', age:35},
+];
+
+const everyPersons = persons.every((person) => person.isOld ===false);
+console.log(everyPersons);
+
+const somePersons = persons.some((person) => person.isOld ===false);
+console.log(somePersons);
+
+
+
